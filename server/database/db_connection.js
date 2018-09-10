d4b96c949d09cc36b9761fea042f148c36c74d10
Fatimat <gbajaf@yahoo.co.uk>
2018-09-10 16:13:18 +0100

    Database setup
    Created one table in database. Use pg-promise to connect to database.
    Database url gotten from heroku and added to environment variables.

---
+++ server/database/db_connection.js
@@ -0,0 +1,26 @@
+const pgp = require("pg-promise")();
+const url = require("url");
+require("env2")("config.env");
+
+if (!process.env.DATABASE_URL) {
+    throw new Error("Environment variable DATABASE_URL must be set.");
+}
+
+const params = url.parse(process.env.DATABASE_URL);
+console.log("PARAMSSSS: ", params);
+const [username, password] = params.auth.split(":");
+
+localDB = {
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split("/")[1],
+    max: process.env.DB_MAX_CONNECTIONS || 2,
+    user: username,
+    password,
+    ssl: params.hostname !== "localhost"
+};
+
+const connection = localDB;
+
+const db = pgp(connection);
+module.exports = db;
