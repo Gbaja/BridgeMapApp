0f6f6a16801a7f90db28e64af4f81a30d17d1340
Fatimat <gbajaf@yahoo.co.uk>
2018-09-18 14:08:59 +0100

    Added Home page and react-router-dom
    Added basic component for Home. Added react-router-dom for easy navigation
    and switching between pages.

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 import LoginContainer from './Login/Login_Container';
 import SignupContainer from './Signup/Signup_Container';
+import Home from './Home/Home';
 
 class App extends Component {
 
   render() {
     return (
-      <div className="App">
-       BridgeMap
-       <LoginContainer />
-       <SignupContainer/>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/login" component={LoginContainer} />
+          <Route exact path="/signup" component={SignupContainer} />
+          <Route exact path="/" component={Home} />
+        </Switch>
+      </Router>
     );
   }
 }
---
+++ src/components/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Link} from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>BridgeMap</h1>
+      <Link to="/login">Log in</Link>
+      <Link to="signup">Sign up</Link>
+    </div>
+  )
+}
+
+export default Home
+
