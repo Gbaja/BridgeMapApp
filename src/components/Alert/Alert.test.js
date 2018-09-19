7e0081541191fd5d6a6b33dd3a482e842300882d
Fatimat <gbajaf@yahoo.co.uk>
2018-09-19 13:09:19 +0100

    Added babelrc and more tests
    All front end tests were failing but it is now working because I added
    babelrc and babel related dependencies.

--- src/components/Alert/Alert.test.js
+++ src/components/Alert/Alert.test.js
@@ -1,19 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import renderer from "react-test-renderer";
+import { shallow } from "enzyme";
 
 import Alert from "./Alert";
 
 describe("App", () => {
-//   test("renders without crashing", () => {
-//     const div = document.createElement("div");
-//     ReactDOM.render(<Alert />, div);
-//     ReactDOM.unmountComponentAtNode(div);
-//   });
+  const alert = {
+    type: "error",
+    message: "heyy"
+  };
+  test("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<Alert alert={alert} />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 
   test("has a valid snapshot", () => {
-    const component = renderer.create(<Alert />);
+    const component = renderer.create(<Alert alert={alert} />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  test("shallow rendering", () => {
+    const wrapper = shallow(<Alert alert={alert} />);
+    expect(wrapper.find("p").length).toBe(1);
   });
 });
--- src/components/Login/Login.test.js
+++ src/components/Login/Login.test.js
@@ -1,16 +1,16 @@
-//import React from "react";
-//import { shallow } from "enzyme";
+import React from "react";
+import { shallow } from "enzyme";
 
-//import LoginForm from "./LoginForm";
+import LoginForm from "./Login_Form";
 
 describe("Login form component", () => {
-  test("it renders without crashing", () => {
-    //const onSubmit = jest.fn();
-    //const handleSubmit = jest.fn();
-    // const wrapper = shallow(
-    //   <LoginForm onSubmit={onSubmit} handleSubmit={handleSubmit} />
-    // );
-    // console.log("LOGIN FORM WRAPPER: ", wrapper);
-    expect(1).toEqual(1);
+  test("it renders a form with length of 1", () => {
+    const onSubmit = jest.fn();
+    const handleSubmit = jest.fn();
+    const wrapper = shallow(
+      <LoginForm onSubmit={onSubmit} handleSubmit={handleSubmit} />
+    );
+    console.log("LOGIN FORM WRAPPER: ", wrapper.find("form"));
+    expect(wrapper.find("form").length).toBe(1);
   });
 });
