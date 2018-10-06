9f06b8250cdcf2188ed8d5c268464272d1a8153d
Fatimat <gbajaf@yahoo.co.uk>
2018-10-06 21:09:29 +0100

    Moved App related into a folder and added it to the routes folder

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 
 import store from "./store";
 import './index.css';
-import App from './routes/App';
+import App from './routes/App/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
